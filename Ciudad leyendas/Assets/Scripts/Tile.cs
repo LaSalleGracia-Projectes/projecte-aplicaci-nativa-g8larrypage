134724443250546b9effc59e24ab08328341e6d6
Pol Barrull <polcris61@gmail.com>
2025-02-21 19:37:22 +0100

    Bases Construcció
    Fetes les bases per afegir construccions basiques

--- Ciudad leyendas/Assets/Scripts/Tile.cs
+++ Ciudad leyendas/Assets/Scripts/Tile.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private Color _baseColor, _offsetColor, _selectedColor;
     [SerializeField] private SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
 
-    public void Init(bool isOffset)
+    private bool _isSelected = false;
+    private bool _hasBuilding = false;
+    private Color _previousColor; // Nuevo: Guardar color anterior
+
+    public void Init(bool isOffset, BuildingPlacer placer)
     {
-        _renderer.color = isOffset ? _offsetColor : _baseColor;
+        _previousColor = isOffset ? _offsetColor : _baseColor;
+        _renderer.color = _previousColor;
     }
 
     void OnMouseEnter()
@@ -21,5 +26,29 @@ public class Tile : MonoBehaviour
     void OnMouseExit()
     {
         _highlight.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if (!_hasBuilding)
+        {
+            FindObjectOfType<BuildingPlacer>().SelectTile(this);
+        }
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+        if (!_hasBuilding)
+        {
+            _isSelected = isSelected;
+            _renderer.color = isSelected ? _selectedColor : _previousColor;
+        }
+    }
+
+    public void SetBuildingColor(Color buildingColor)
+    {
+        _renderer.color = buildingColor;
+        _hasBuilding = true;
+        _previousColor = buildingColor;
     }
 }
