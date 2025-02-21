67fd4fad73800cd2ed8ae08f79737822516ff779
Óscar Gómez <oscargomezsedas@gmail.com>
2025-02-21 18:57:09 +0100

    feat: add login form components to FormManager

--- Ciudad leyendas/Assets/Scripts/FormManager.cs
+++ Ciudad leyendas/Assets/Scripts/FormManager.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class FormManager : MonoBehaviour
 {
+    [Header("LogIn Form")]
+    public GameObject logInForm;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public Button logInButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
