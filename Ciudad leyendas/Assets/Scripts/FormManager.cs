5bdfcaceec49f8d82fb379a28fdb69f92f098380
Óscar Gómez <oscargomezsedas@gmail.com>
2025-02-25 16:29:00 +0100

    feat: add Google sign-in functionality to FormManager and update build settings

--- Ciudad leyendas/Assets/Scripts/FormManager.cs
+++ Ciudad leyendas/Assets/Scripts/FormManager.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Threading.Tasks;
 using Supabase;
-using UnityEngine;
+using Supabase.Gotrue;
+using Supabase.Gotrue.Exceptions;
 using TMPro;
-using UnityEngine.Events;
-using UnityEngine.Rendering;
-using UnityEngine.Serialization;
+using UnityEngine;
 using UnityEngine.UI;
+using System.Net;
+using Client = Supabase.Client;
 
 public class FormManager : MonoBehaviour
 {
@@ -16,20 +17,121 @@ public class FormManager : MonoBehaviour
     public TMP_InputField passwordInput;
     public Button logInButton;
     public TMP_Text infoText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button logInGoogleButton;
+
+    private bool _doSignIn;
+    private bool _doExchangeCode;
+    private HttpListener httpListener;
+    private string _pkce;
+    private string _token;
+
     void Start()
     {
         infoText.text = "";
         logInButton.onClick.AddListener(LogIn);
+        logInGoogleButton.onClick.AddListener(SignInWithGoogle);
     }
 
-
-    // Update is called once per frame
-    void Update()
+    private async void SignInWithGoogle()
     {
-        
+        _doSignIn = true;
     }
-    
+
+    private async void Update()
+    {
+        if (_doSignIn)
+        {
+            _doSignIn = false;
+            await PerformSignIn();
+        }
+
+        if (_doExchangeCode)
+        {
+            _doExchangeCode = false;
+            await PerformExchangeCode();
+        }
+    }
+
+    private void StartLocalWebserver()
+    {
+        if (httpListener == null)
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://*:3000/");
+            httpListener.Start();
+            httpListener.BeginGetContext(new AsyncCallback(IncomingHttpRequest), httpListener);
+        }
+    }
+
+    private void IncomingHttpRequest(IAsyncResult result)
+    {
+        HttpListener httpListener = (HttpListener)result.AsyncState;
+        HttpListenerContext httpContext = httpListener.EndGetContext(result);
+        HttpListenerRequest httpRequest = httpContext.Request;
+
+        _token = httpRequest.QueryString.Get("code");
+
+        HttpListenerResponse httpResponse = httpContext.Response;
+        string responseString = "<html><body><b>DONE!</b><br>(You can close this tab/window now)</body></html>";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+        httpResponse.ContentLength64 = buffer.Length;
+        System.IO.Stream output = httpResponse.OutputStream;
+        output.Write(buffer, 0, buffer.Length);
+        output.Close();
+
+        httpListener.Stop();
+        httpListener = null;
+        _doExchangeCode = true;
+    }
+
+    private async Task PerformSignIn()
+    {
+        try
+        {
+            var supabase = await ConnectSupabase();
+            var providerAuth = (await supabase.Auth.SignIn(Constants.Provider.Google, new SignInOptions
+            {
+                FlowType = Constants.OAuthFlowType.PKCE,
+            }))!;
+            _pkce = providerAuth.PKCEVerifier;
+
+            StartLocalWebserver();
+            Application.OpenURL(providerAuth.Uri.ToString());
+        }
+        catch (GotrueException goTrueException)
+        {
+            infoText.text = $"{goTrueException.Reason} {goTrueException.Message}";
+            infoText.color = Color.red;
+            Debug.Log(goTrueException.Message, gameObject);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message, gameObject);
+        }
+    }
+
+    private async Task PerformExchangeCode()
+    {
+        try
+        {
+            var supabase = await ConnectSupabase();
+            Session session = (await supabase.Auth.ExchangeCodeForSession(_pkce, _token)!);
+            infoText.text = $"Success! Signed Up as {session.User?.Email}";
+            infoText.color = Color.green;
+        }
+        catch (GotrueException goTrueException)
+        {
+            infoText.text = $"{goTrueException.Reason} {goTrueException.Message}";
+            infoText.color = Color.red;
+            Debug.Log(goTrueException.Message, gameObject);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message, gameObject);
+        }
+    }
+
     private static async Task<Client> ConnectSupabase()
     {
         var url = SupabaseKeys.supabaseURL;
@@ -39,14 +141,14 @@ public class FormManager : MonoBehaviour
         {
             AutoConnectRealtime = true
         };
-        
+
         var supabase = new Client(url, key, options);
 
         await supabase.InitializeAsync();
-        
+
         return supabase;
     }
-    
+
     private async void LogIn()
     {
         try
@@ -63,7 +165,6 @@ public class FormManager : MonoBehaviour
                 Debug.Log("LogIn successful!");
                 infoText.text = "LogIn successful!";
                 infoText.color = Color.green;
-                // Add additional logic for successful login
             }
         }
         catch (Exception e)
