1f4d5b96807992d1e16f8ede92694136e3dfdf22
Óscar Gómez <oscargomezsedas@gmail.com>
2025-02-28 18:59:29 +0100

    feat: enhance FormManager with sign-up functionality and navigation

--- Ciudad leyendas/Assets/Scripts/FormManager.cs
+++ Ciudad leyendas/Assets/Scripts/FormManager.cs
@@ -8,19 +8,35 @@ using Client = Supabase.Client;
 
 public class FormManager : MonoBehaviour
 {
-    [Header("LogIn Form")] public GameObject logInForm;
+    [Header("LogIn Form")]
+    public GameObject logInForm;
     public TMP_InputField emailInput;
     public TMP_InputField passwordInput;
     public Button logInButton;
     public TMP_Text infoText;
     public Button logInGoogleButton;
     public GoogleSignInManager googleSignInManager;
+    public Button goToSignUpForm;
+    [Header("SignUp Form")]
+    public GameObject signUpForm;
+    public TMP_InputField signUpEmailInput;
+    public TMP_InputField signUpEmailConfirmInput;
+    public TMP_InputField signUpPasswordInput;
+    public TMP_InputField signUpPasswordConfirmInput;
+    public Button signUpButton;
 
     void Start()
     {
+        logInForm.SetActive(true);
+        signUpForm.SetActive(false);
         infoText.text = "";
         logInButton.onClick.AddListener(LogIn);
         logInGoogleButton.onClick.AddListener(googleSignInManager.SignInWithGoogle);
+        goToSignUpForm.onClick.AddListener(() =>
+        {
+            logInForm.SetActive(false);
+            signUpForm.SetActive(true);
+        });
     }
 
     private async void LogIn()
