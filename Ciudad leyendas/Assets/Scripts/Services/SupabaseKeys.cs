3a251135a1efba848837c2cf3476f6d244d1e913
Óscar Gómez <oscargomezsedas@gmail.com>
2025-03-09 17:21:41 +0100

    feat: initialize SupabaseManager and update project settings

---
+++ Ciudad leyendas/Assets/Scripts/Services/SupabaseKeys.cs
@@ -0,0 +1,5 @@
+﻿public class SupabaseKeys
+{
+    public static string supabaseURL = "https://zlpjwwiqyssqrzlwinkj.supabase.co";
+    public static string supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpscGp3d2lxeXNzcXJ6bHdpbmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NzQ2NzUsImV4cCI6MjA1MzA1MDY3NX0.ju7cIcFBP7cFD3I9e-F1s1hgHgOJMtOS6AHGaEngcWM";
+}
