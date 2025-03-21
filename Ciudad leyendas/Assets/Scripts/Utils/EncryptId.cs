a1cb3db94193a435531743e6fbfe3cf23969ceb2
Óscar Gómez <oscargomezsedas@gmail.com>
2025-03-21 19:38:38 +0100

    feat: implement GetAndroidId method for retrieving Android device ID

--- Ciudad leyendas/Assets/Scripts/Utils/EncryptId.cs
+++ Ciudad leyendas/Assets/Scripts/Utils/EncryptId.cs
@@ -1,7 +1,35 @@
-﻿namespace Utils
+﻿using UnityEngine;
+
+namespace Utils
 {
     public class EncryptId
     {
-        
+        private string _androidId;
+
+        public string GetAndroidId()
+        {
+            if (!string.IsNullOrEmpty(_androidId))
+                return _androidId;
+
+            #if UNITY_ANDROID && !UNITY_EDITOR
+                using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+                {
+                    using (var currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                    {
+                        using (var contentResolver = currentActivity.Call<AndroidJavaObject>("getContentResolver"))
+                        {
+                            using (var secure = new AndroidJavaClass("android.provider.Settings$Secure"))
+                            {
+                                _androidId = secure.CallStatic<string>("getString", contentResolver, "android_id");
+                            }
+                        }
+                    }
+                }
+            #else
+            _androidId = SystemInfo.deviceUniqueIdentifier;
+            #endif
+
+            return _androidId;
+        }
     }
 }
