2b403d28a5964e11a4460c1d72518215024e2f2f
Óscar Gómez <oscargomezsedas@gmail.com>
2025-03-21 19:34:55 +0100

    feat: add EncryptId utility class and TemporalData model for database interaction

---
+++ Ciudad leyendas/Assets/Scripts/Models/TemporalData.cs
@@ -0,0 +1,24 @@
+using System;
+using Supabase.Postgrest.Attributes;
+
+namespace Models
+{
+    [Table("temporal_data")]
+    public class TemporalData
+    {
+        [PrimaryKey("id")]
+        public Guid Id { get; set; }
+        
+        [Column("android_id")]
+        public string AndroidId { get; set; }
+        
+        [Column("pasos_totales")]
+        public int PasosTotales { get; set; }
+        
+        [Column("nuevos_pasos_sync")]
+        public int NuevosPasosSync { get; set; }
+        
+        [Column("salt")]
+        public string Salt { get; set; }
+    }
+}
