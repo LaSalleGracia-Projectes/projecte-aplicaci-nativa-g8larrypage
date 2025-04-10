b8a1f03f4f1d11a75853595c8922f933ae2f7bff
Óscar Gómez <oscargomezsedas@gmail.com>
2025-04-10 15:26:57 +0200

    feat: add Usuario class for user management with properties for role, name, email, and last connection

---
+++ Ciudad leyendas/Assets/Scripts/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace Models
+{
+    public class Usuario : BaseModel
+    {
+        [PrimaryKey("id")]
+        public Guid Id { get; set; }
+        
+        [Column("role")]
+        public string Role { get; set; }
+        
+        [Column("nombre")]
+        public string Nombre { get; set; }
+        
+        [Column("correo")]
+        public string Correo { get; set; }
+        
+        [Column("last_connexion")]
+        public DateTime LastConnexion { get; set; }
+    }
+}
