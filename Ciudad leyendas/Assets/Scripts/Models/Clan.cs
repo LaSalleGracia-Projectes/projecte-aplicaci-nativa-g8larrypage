300de1b9ad3e07e49ef7b477a745ebbe55af6852
Óscar Gómez <oscargomezsedas@gmail.com>
2025-04-11 19:29:19 +0200

    feat: add Clan class for managing clan data with properties for ID, name, leader ID, and total experience

---
+++ Ciudad leyendas/Assets/Scripts/Models/Clan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace Models
+{
+    [Table("Clan")]
+    public class Clan: BaseModel
+    {
+        [PrimaryKey("id_clan")]
+        public int IdClan { get; set; }
+        
+        [Column("nombre")]
+        public string Nombre { get; set; }
+        
+        [Column("id_leader")]
+        public Guid IdLeader { get; set; }
+        
+        [Column("experiencia_total")]
+        public int ExperienciaTotal { get; set; }
+        
+        
+    }
+}
